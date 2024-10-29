5d75a27ba6eb32a3e89970a93923e46ac208fe96
Dipak Bishwas <dipakbishwas08@gmail.com>
2024-10-30 01:12:07 +0530

    add / in project

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,10 +3,10 @@
 import pandas as pd
 import joblib
 
-model_young = joblib.load("artifacts\model_young.joblib")
-model_rest = joblib.load("artifacts\model_young_rest.joblib")
-scaler_young = joblib.load("artifacts\scaler_young.joblib")
-scaler_rest = joblib.load("artifacts\scaler_young_rest.joblib")
+model_young = joblib.load("artifacts/model_young.joblib")
+model_rest = joblib.load("artifacts/model_young_rest.joblib")
+scaler_young = joblib.load("artifacts/scaler_young.joblib")
+scaler_rest = joblib.load("artifacts/scaler_young_rest.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
